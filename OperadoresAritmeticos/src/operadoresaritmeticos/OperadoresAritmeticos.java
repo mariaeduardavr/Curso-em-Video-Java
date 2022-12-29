bfe6019b7fb8759b0f34fa06312bf2dec5c7d65d
Maria Eduarda <107825119+mariaeduardavr@users.noreply.github.com>
2022-12-29 15:27:57 -0300

    #07 - Operadores Aritméticos e Classe Math

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,24 @@
+package operadoresaritmeticos;
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+        /*int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A média é igual a " + m);*/
+        
+        /*int numero = 5;
+        int valor = 5 + numero++;
+        System.out.println(valor);
+        System.out.println(numero);*/
+        
+        /*int x = 4;
+        x *= 2;
+        System.out.println(x);*/
+        
+        float v = 8.9f;
+        int ar = (int) Math.floor(v);
+        System.out.println(ar);
+    }
+    
+}
